5b252e4a1e024c82892dd229005588832a6dbbe9
Pang Yen Wu <wupangye@buffalo.edu>
2021-02-09 19:11:53 -0500

    Add 2 title

---
+++ Udder/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Udder
+//
+//  Created by Pang Yen Wu on 2/9/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Udder")
+            .padding()
+        Text("Fresh Milk On The Way")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
